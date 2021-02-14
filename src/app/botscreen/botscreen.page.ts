8e65c619ef404e76c741905a7dac55256bfdbbae
rajaishwary <rajaishwary@gmail.com>
2021-02-14 05:41:13 +0530

    update bot questions after user response

--- src/app/botscreen/botscreen.page.ts
+++ src/app/botscreen/botscreen.page.ts
@@ -8,12 +8,80 @@ import { conversations } from '../../mock';
 })
 export class BotscreenPage implements OnInit {
   conversations: any;
+  currentStep: number;
+  currentStepObj: any;
+  inputVal: string;
 
-  constructor() { 
-    this.conversations = conversations;
+  constructor() {
+    this.conversations = [];
+    this.currentStep = 1;
+    this.currentStepObj = conversations.find(item => item.step === this.currentStep);
+    this.inputVal = '';
   }
 
   ngOnInit() {
+    this.conversations = this.getConversationsToStep(this.currentStep);
   }
 
+  onChangeInput(e) {
+    this.inputVal = e.target.value;
+  }
+
+  onSubmitBtn() {
+    console.log(this.inputVal);
+    this.updateUserStep(1);
+  }
+
+  getConversationsToStep(step) {
+    let showConvs = [];
+    conversations.forEach(item => {
+      if (item.step <= step) showConvs.push(item);
+    });
+    return showConvs;
+  }
+
+  getStepObj(step) {
+    return conversations.find(item => item.step === step);
+  }
+
+  updateUserStep(nStep) {
+    const nextStep = this.currentStep + nStep;
+    const nextStepObjInConversations = this.getStepObj(nextStep);
+    const updatedConversations = this.conversations.splice(0);
+    updatedConversations.push(nextStepObjInConversations);
+    updatedConversations.splice(nextStep, 1, { ...nextStepObjInConversations, message: this.inputVal });
+    this.conversations = updatedConversations;
+    this.currentStep = nextStep;
+    this.currentStepObj = { ...nextStepObjInConversations, message: this.inputVal };
+    this.updateBotStep(1);
+  }
+
+  updateBotStep(nStep) {
+    const nextStep = this.currentStep + nStep;
+    const nextStepObjInConversations = this.getStepObj(nextStep);
+    const updatedConversations = this.conversations.splice(0);
+    updatedConversations.push(nextStepObjInConversations);
+    updatedConversations.splice(nextStep, 1, { ...nextStepObjInConversations, loading: true });
+    this.conversations = updatedConversations;
+    this.currentStep = nextStep;
+    this.currentStepObj = { ...nextStepObjInConversations, loading: true };
+    setTimeout(() => {
+      this.updateBotQuestion();
+    }, 2000);
+  }
+
+  updateBotQuestion() {
+    const nextStep = this.currentStep;
+    const nextStepObjInConversations = this.getStepObj(nextStep);
+    const updatedConversations = this.conversations.splice(0);
+    updatedConversations.splice(nextStep, 1, { ...nextStepObjInConversations, loading: false });
+    this.conversations = updatedConversations;
+    this.currentStep = nextStep;
+    this.currentStepObj = { ...nextStepObjInConversations, loading: false };
+    const checkIfNextIsBot = this.getStepObj(nextStep + 1);
+    this.inputVal = '';
+    if (checkIfNextIsBot.user === 'bot') {
+      this.updateBotStep(1);
+    }
+  }
 }
--- src/mock/index.js
+++ src/mock/index.js
@@ -1,6 +1,6 @@
 export const conversations = [
     {
-        step: 1,
+        step: 0,
         user: 'bot',
         message: `Hey ! I'm Bella the bot. \n Let's get to know each other! :)`,
         path: 'src/assets/bella-profile.png',
@@ -8,53 +8,53 @@ export const conversations = [
 
     },
     {
-        step: 2,
+        step: 1,
         user: 'bot',
         message: `What's your name ?`,
         path: 'src/assets/bella-profile.png',
         responseType: 'text'
     },
     {
-        step: 3,
+        step: 2,
         user: 'user',
         message: '',
         path: ''
     },
     {
-        step: 4,
+        step: 3,
         user: 'bot',
         message: `Nice to meet you, Jane! How old are you ?`,
         path: 'src/assets/bella-profile.png',
-        responseType: 'number'
+        responseType: 'text'
     },
     {
-        step: 5,
+        step: 4,
         user: 'user',
         message: '',
         path: ''
     },
     {
-        step: 6,
+        step: 5,
         user: 'bot',
         message: `I talk to people for a living.`,
         path: 'src/assets/bella-profile.png',
         responseType: 'none'
     },
     {
-        step: 7,
+        step: 6,
         user: 'bot',
         message: `What do you do?`,
         path: 'src/assets/bella-profile.png',
-        responseType: 'dropdown'
+        responseType: 'text'
     },
     {
-        step: 8,
+        step: 7,
         user: 'user',
         message: '',
         path: ''
     },
     {
-        step: 9,
+        step: 8,
         user: 'bot',
         message: `Thanks! That's all i need`,
         path: 'src/assets/bella-profile.png',
